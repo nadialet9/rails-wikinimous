1b2ef5110530517d44b74e7cb732ae9bd1362d63
n.l <n.leteurtre@gmail.com>
2020-11-17 17:01:14 +0100

    add wiki.. exercize

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  title = Faker::Lorem.sentence(word_count: 3)
+  content = Faker::Lorem.sentence(20)
+  Article.create(title: title, content: content)
+end
